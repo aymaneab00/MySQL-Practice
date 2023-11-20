d26f4c0e2bc986c733edb12856fa8e86ca3202f5
aymaneab00 <soquavo@gmail.com>
2023-11-20 22:10:22 +0000

    #

--- Serie Transaction/transaction try.sql
+++ Serie Transaction/transaction try.sql
@@ -13,26 +13,24 @@ insert into banc  values('ac1',30000),('ac2',40000);
 drop procedure if exists virement ;
 delimiter $$
 create procedure virement (acc1 varchar (50), acc2 varchar(50) , amount float)
-
 begin
 declare flag boolean default false;
+begin
 declare exit handler for sqlexception
 begin
 rollback;
 set flag =true; 
 end;
 start transaction;
-update banc set montant = montant -amount where ac1=ac1;
-update banc set montant = montant +amount where ac2=ac2;
-select 'virement effectue';
-
+update banc set montant = montant -amount where accname=acc1;
+update banc set montant = montant +amount where accname=acc2;
 commit;
+select 'virement effectue';
+end ;
 if flag then 
 select 'virement annulé';
-
 end if;
 end$$
-
 delimiter ;
-call virement ('ac1','ac2',10000);
+call virement ('ac1','ac2',5000);
 select*from banc;
